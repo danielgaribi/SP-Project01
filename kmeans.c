622694bdc69da6f4e44698aac619d384c7df480f
danielgaribi <danielgaribi@mail.tau.ac.il>
2021-05-02 18:50:48 +0300

    Start C Side

---
+++ kmeans.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+#define DEFAULT_MAX_ITER 200
+#define DEBUG_INPUT
+
+void kmean(int **pointsArray, int k, int max_iter);
+int readArgs(int argc, char *argv[], int* k, int* max_iter);
+void readPointsArray();
+void stringToPoint(char *str);
+
+int main(int argc, char *argv[]) {
+    assert(2 <= argc && argc <= 3);
+    int k, max_iter;
+    int res = readArgs(argc, argv, &k, &max_iter);
+    if(res == false) {
+        return 0;
+    }
+
+    readPointsArray();
+
+    return 0;
+}
+
+int readArgs(int argc, char *argv[], int* k, int* max_iter) {
+    *k = atoi(argv[1]); /////////////////////////////////////////////////////////////////////
+    if (*k == 0) {
+        printf("K is not a valid integer, exits...\n");
+        return false;
+    }
+
+    *max_iter = argc == 3 ? atoi(argv[2]) : DEFAULT_MAX_ITER;
+    if (*max_iter == 0) {
+        printf("max_iter is not a valid integer, exits...\n");
+        return false;
+    }
+
+    #ifdef DEBUG_INPUT
+    printf("K: %d \nmax_iter: %d\n", *k, *max_iter);
+    #endif
+
+    return true;
+}
+
+void readPointsArray() {
+    char str[1024];
+    int d = 0;
+    double value;
+    char c;
+    while(scanf("%lf%c", &value, &c) == 2) {
+        d++;
+        if(c == '\n') {
+            break;
+        }
+    }
+    
+    int i;
+
+    while(scanf("%lf%c", &value, &c) == 2) {
+
+    }
+
+    for (int i =0; i< 9; i++)
+    {
+        printf("\n");
+        scanf("%s", str);
+         
+        stringToPoint(str);
+
+    }    
+}
+
+void stringToPoint(char *str){
+    int init_size = strlen(str);
+    char delim[] = ",";
+    char *ptr = strtok(str, delim);
+    double value;
+    while(ptr != NULL)
+	{ 
+        sscanf(ptr, "%lf", &value);
+		printf("'%lf'\n", value);
+		ptr = strtok(NULL, delim);
+	}
+}
+
+void kmean(int **pointsArray, int k, int max_iter) {
+    int **centroids;
+}
